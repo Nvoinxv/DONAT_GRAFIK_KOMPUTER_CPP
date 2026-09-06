9c9d5ea9e1e520b33fd0291bd08c3acd9bda05ff
Nvoinxv <sukinvoin67@gmail.com>
2026-09-06 16:46:25 +0800

    penambahan pada logika main.cpp

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include "include/render_ascii.hpp"
+#include "include/membuat_geomteris_donat.hpp"
+#include <chrono>
+#include <thread>
+
+int main() {
+    render_ascii render;
+    Donat_Geometris donat;
+
+    while (true) {
+        std::cout << "\033[2J\033[H"; 
+        donat.rotasi(2);
+        render.layar(donat);
+        std::this_thread::sleep_for(std::chrono::miliseconds(5));
+    }
+}
